bcb39f563fd7bdc6cdf56fbb85047f1fbcddeb6a
Pere Bohigas <pere.bohigas@gmail.com>
2024-03-24 02:43:12 +0100

    Fix/split into framework and executable (#6)
    * Fix usages of arguments property
    
    * Add filePaths property

--- Sources/SwiftPolyglot/main.swift
+++ Sources/SwiftPolyglot/main.swift
@@ -1,6 +1,17 @@
+import Foundation
 import SwiftPolyglotCore
 
-let swiftPolyglot: SwiftPolyglot = .init(arguments: CommandLine.arguments)
+guard
+    let enumerator = FileManager.default.enumerator(atPath: FileManager.default.currentDirectoryPath),
+    let filePaths = enumerator.allObjects as? [String]
+else {
+    exit(EXIT_FAILURE)
+}
+
+let swiftPolyglot: SwiftPolyglot = .init(
+    arguments: Array(CommandLine.arguments.dropFirst()),
+    filePaths: filePaths
+)
 
 do {
     try swiftPolyglot.run()
