331b8821ee0aa78f74e9e3f79c94fd163626053a
Pere Bohigas <pere.bohigas@gmail.com>
2024-03-24 20:51:59 +0100

    Add test for checking a string catalog with missing translations

--- Tests/SwiftPolyglotCoreTests/SwiftPolyglotCoreTests.swift
+++ Tests/SwiftPolyglotCoreTests/SwiftPolyglotCoreTests.swift
@@ -21,5 +21,25 @@ final class SwiftPolyglotCoreTests: XCTestCase {
 
         XCTAssertNoThrow(try swiftPolyglot.run())
     }
+
+    func testStringCatalogWithMissingTranslations() throws {
+        guard
+            let stringCatalogFilePath = Bundle.module.path(
+                forResource: "WithMissingTranslations",
+                ofType: ".xcstrings",
+                inDirectory: "TestFiles"
+            )
+        else {
+            XCTFail("String catalog with missing translations for testing not found")
+            return
+        }
+
+        let swiftPolyglot: SwiftPolyglot = .init(
+            arguments: ["ca,de,en,es", "--errorOnMissing"],
+            filePaths: [stringCatalogFilePath]
+        )
+
+        XCTAssertThrowsError(try swiftPolyglot.run())
+    }
 }
 
