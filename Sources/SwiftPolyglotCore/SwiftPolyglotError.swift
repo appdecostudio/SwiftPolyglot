a86446fe60240c1c1738713c3ee2cfc10e5f2e6c
Pere Bohigas <pere.bohigas@gmail.com>
2024-04-01 21:58:16 +0200

    Format

--- Sources/SwiftPolyglotCore/SwiftPolyglotError.swift
+++ Sources/SwiftPolyglotCore/SwiftPolyglotError.swift
@@ -8,10 +8,10 @@ enum SwiftPolyglotError: Error {
 extension SwiftPolyglotError: LocalizedError {
     public var errorDescription: String? {
         switch self {
-            case .missingTranslations:
-                return "Error: One or more translations are missing."
-            case let .unsupportedVariation(variation):
-                return "Variation type '\(variation)' is not supported. Please create an issue in GitHub"
+        case .missingTranslations:
+            return "Error: One or more translations are missing."
+        case let .unsupportedVariation(variation):
+            return "Variation type '\(variation)' is not supported. Please create an issue in GitHub"
         }
     }
 }
